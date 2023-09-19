22c56f1d5e2bae9841f2138ba57b112b34b24d3a
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-19 13:13:20 +0500

    Добавьте файлы проекта.

---
+++ ex29/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetNumber();
+        }
+
+        static int GetNumber()
+        {
+            Console.WriteLine("Введите число: ");
+            var input = Console.ReadLine();
+
+            if (int.TryParse(input, out int value))
+            {
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение!");
+            }
+        }
+    }
+}
